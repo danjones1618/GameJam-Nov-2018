8e65ffb0d7a0d21bf5e6362461ac183454fec6a3
viper12590 <viper1259@gmail.com>
2018-11-19 14:20:15 +0000

    player movement done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,16 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 10f;
     public bool isJump = false;
 
+    public enum RotationAxes { MouseXandY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXandY;
+    public float sensX = 3F;
+    public float sensY = 3F;
+    public float minX = -360F;
+    public float maxX = 360F;
+    public float minY = -60F;
+    public float maxY = 60F;
+    public float rotationY = 0f;
+
     // Use this for initialization
     void Start()
     {
@@ -21,16 +31,16 @@ public class PlayerMovement : MonoBehaviour
     {
 
         // W, A, S, D movments
-        if (Input.GetKey("w")) {
+        if (Input.GetKey("a")) {
             transform.Translate(speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("s")) {
+        if (Input.GetKey("d")) {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("a")) {
+        if (Input.GetKey("w")) {
             transform.Translate(0, 0, speed * Time.deltaTime);
         }
-        if (Input.GetKey("d")) {
+        if (Input.GetKey("s")) {
             transform.Translate(0, 0, -speed * Time.deltaTime);
         }
 
@@ -40,11 +50,32 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(0, jumpForce * Time.deltaTime, 0);
             isJump = true;
         }
-
+        
         //Turning head
+        if (axes == RotationAxes.MouseXandY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensX;
 
+            rotationY += Input.GetAxis("Mouse Y") * sensY;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensY;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
 
     }
+
+
     //jump update
     private void OnCollisionEnter(Collision col)
     {
