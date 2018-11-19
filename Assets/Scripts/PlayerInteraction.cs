6fdee742a6bbcf32a5c6397a5586e7bd95b7b616
viper12590 <viper1259@gmail.com>
2018-11-19 16:56:12 +0000

    simple interaction added

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    public Camera fpsCam;
+    public float range = 2f;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("f"))
+        {
+            Interact();
+        }
+	}
+
+    void Interact()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
